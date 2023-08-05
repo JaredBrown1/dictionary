ab75f8bdbf38b01ea6a71f8148cec6b70fc86d2b
Jared Brown <jaredbrown1@hotmail.com>
2023-08-04 20:57:05 -0700

    synonyms rendering

--- src/app/components/word/nounmeaning/Definition.jsx
+++ src/app/components/word/nounmeaning/Definition.jsx
@@ -11,7 +11,7 @@ const Definition = ({ definition }) => {
 
 	if (definitions.length === 1) {
 		return (
-			<div className="flex justify-start flex-col max-w-2xl">
+			<div className="flex justify-start flex-col w-[736px]">
 				<div className="flex pt-4">
 					<h1 className="pr-4 text-xl font-bold">{partOfSpeech}</h1>
 					<div className="h-1 w-full mt-3 bg-gray-200"></div>
@@ -26,8 +26,10 @@ const Definition = ({ definition }) => {
 		);
 	}
 
+	let id = 0;
+
 	return (
-		<div className="flex justify-start flex-col max-w-2xl">
+		<div className="flex justify-start flex-col w-[736px]">
 			<div className="flex pt-4">
 				<h1 className="pr-4 text-xl font-bold">{partOfSpeech}</h1>
 				<div className="h-1 w-full mt-3 bg-gray-200"></div>
@@ -36,12 +38,24 @@ const Definition = ({ definition }) => {
 				<h1 className="text-gray-400 text-xl">Meaning</h1>
 				<div className="pt-6 pl-10">
 					<ul className="list-disc marker:text-purple-600">
-						{definitions.map((def) => (
+						{definitions.slice(0, 3).map((def) => (
 							<li key={def.definition} className="p-4">
 								{def.definition}
 							</li>
 						))}
 					</ul>
+				</div>
+				<div className="flex justifyå-between pt-10">
+					<h1 className="text-gray-400 text-xl pr-4">Synonyms</h1>
+					<div className="flex text-purple-600 text-xl font-bold">
+						{definition.meanings
+							? definition.meanings[0].synonyms.slice(0, 5).map((synonym) => (
+									<h1 className="pl-2 pr-2" key={id++}>
+										{synonym}
+									</h1>
+							  ))
+							: null}
+					</div>
 				</div>
 			</div>
 		</div>
@@ -49,37 +63,3 @@ const Definition = ({ definition }) => {
 };
 
 export default Definition;
-
-// const Definition = ({ definition }) => {
-// 	if (!definition || !definition.meanings || definition.meanings.length === 0) {
-// 		return null;
-// 	}
-
-// 	return (
-// 		<div className="flex justify-start flex-col">
-// 			<div className="flex pt-4">
-// 				<h1 className="pr-4 text-xl font-bold">
-// 					{definition.meanings[0].partOfSpeech}
-// 				</h1>
-// 				<div className="h-1 w-full mt-3 bg-gray-200"></div>
-// 			</div>
-// 			<div className="pt-6">
-// 				<h1 className="text-gray-400 text-xl">Meaning</h1>
-// 				<div className="pt-6 pl-10">
-// 					<ul className="list-disc marker:text-purple-600">
-// 						<li className="p-4">
-// 							{definition.meanings[0].definitions[0].definition}
-// 						</li>
-// 						<li className="p-4">
-// 							{definition.meanings[0].definitions[1].definition}
-// 						</li>
-// 						{/* <li className="p-4">
-// 							{definition.meanings[0].definitions[2].definition}
-// 						</li> */}
-// 					</ul>
-// 				</div>
-// 			</div>
-// 		</div>
-// 	);
-// };
-// export default Definition;
