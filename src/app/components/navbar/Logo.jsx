c7f0c953288f5b3509d620b85b844bfd8e1ca8b3
Jared Brown <jaredbrown1@hotmail.com>
2023-08-09 18:50:11 -0700

    page refreshes once logo is clicked

--- src/app/components/navbar/Logo.jsx
+++ src/app/components/navbar/Logo.jsx
@@ -3,9 +3,19 @@ import Image from "next/image";
 import Icon from "public/images/logo.svg";
 
 const Logo = () => {
+	const refreshPage = () => {
+		window.location.reload();
+	};
+
 	return (
-		<div className="">
-			<Image src={Icon} alt="Logo" width={28} height={32} />
+		<div className="hover:cursor-pointer">
+			<Image
+				src={Icon}
+				alt="Logo"
+				width={28}
+				height={32}
+				onClick={refreshPage}
+			/>
 		</div>
 	);
 };
